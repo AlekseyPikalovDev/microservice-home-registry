ce1edde2a46b9fb1dfc20b405c0ad51f5338a40a
Aleksey Pikalov <apikalov@bellintegrator.com>
2022-03-13 23:50:23 +0300

    add full CRUD for address entity

--- src/main/kotlin/com/example/home/registry/controller/AddressController.kt
+++ src/main/kotlin/com/example/home/registry/controller/AddressController.kt
@@ -2,22 +2,35 @@ package com.example.home.registry.controller
 
 import com.example.home.registry.dto.AddressDto
 import com.example.home.registry.service.AddressService
+import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.DeleteMapping
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.PutMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-@RequestMapping("/application")
+@RequestMapping("/address")
 class AddressController(
     val addressService: AddressService
 ) {
-
     @PostMapping
-    fun create(@RequestBody addressDto: AddressDto): ResponseEntity<AddressDto> {
-        return ResponseEntity.ok(
-            addressService.create(addressDto)
-        )
-    }
+    fun create(@RequestBody dto: AddressDto) =
+        ResponseEntity(addressService.create(dto), HttpStatus.CREATED)
+
+    @GetMapping("/{id}")
+    fun getById(@PathVariable("id") id: Long) = addressService.getById(id)
+
+    @GetMapping
+    fun getAll() = addressService.getAll()
+
+    @DeleteMapping("/{id}")
+    fun deleteById(@PathVariable("id") id: Long) = addressService.deleteById(id)
+
+    @PutMapping
+    fun update(@RequestBody dto: AddressDto) = addressService.update(dto)
 }
--- src/main/kotlin/com/example/home/registry/service/AddressService.kt
+++ src/main/kotlin/com/example/home/registry/service/AddressService.kt
@@ -3,6 +3,7 @@ package com.example.home.registry.service
 import com.example.home.registry.db.repository.AddressRepository
 import com.example.home.registry.dto.AddressDto
 import com.example.home.registry.mapper.AddressMapper
+import org.springframework.data.repository.findByIdOrNull
 import org.springframework.stereotype.Service
 
 @Service
@@ -17,4 +18,37 @@ class AddressService(
             )
         )
 
+    fun getById(id: Long): AddressDto =
+        mapper.toDto(
+            addressRepository.findByIdOrNull(id)
+                ?: error("Entity with id = $id is not found!")
+        )
+
+    fun getAll(): List<AddressDto> =
+        addressRepository.findAll().map {
+            mapper.toDto(it)
+        }.sortedBy { it.id }
+
+    fun deleteById(id: Long) {
+        addressRepository.findByIdOrNull(id)
+            ?: error("Entity with id = $id is not found!")
+        addressRepository.deleteById(id)
+    }
+
+    fun update(dto: AddressDto): AddressDto =
+        mapper.toDto(
+            addressRepository.save(
+                addressRepository.getById(
+                    dto.id ?: error("Entity with id = ${dto.id} is not found!")
+                ).apply {
+                    country = dto.country
+                    city = dto.city
+                    streetName = dto.streetName
+                    buildingNumber = dto.buildingNumber
+                    flatNumber = dto.flatNumber
+                    zipCode = dto.zipCode
+                    addressType = dto.addressType
+                }
+            )
+        )
 }
